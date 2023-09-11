bd861bec9f63017842cd30c78e118c7521f46b90
symbbad <symbbad@gmail.com>
2023-09-11 17:28:21 +0900

    feat: add view

--- project/app/views.py
+++ project/app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+#test
+def board(request):
+    return render(request, 'board.html')
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def client_board(request):
+    return render(request, 'client_board.html')
+
+def admin_board(request):
+    return render(request, 'admin_board.html')
+
+def write(request):
+    return render(request, 'write.html')
